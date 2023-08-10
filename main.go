8fd0616d5c34eb98a5004d404ec836e8573fe0e7
Habba002 <habib.bushra002@gmail.com>
2023-08-10 16:02:51 +0300

    peer to peer server

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
-	"github.com/habibbushira/ggpoker/deck"
+	server "github.com/habibbushira/ggpoker/p2p"
 )
 
 func main() {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < 10; i++ {
-		d := deck.New()
-		fmt.Println(d)
-		fmt.Println()
+	// rand.New(rand.NewSource(time.Now().UnixNano()))
+	// for i := 0; i < 10; i++ {
+	// 	d := deck.New()
+	// 	fmt.Println(d)
+	// 	fmt.Println()
+	// }
+
+	cfg := server.ServerConfig{
+		ListenAddr: ":3000",
 	}
+	server := server.NewServer(cfg)
+
+	server.Start()
 
 }

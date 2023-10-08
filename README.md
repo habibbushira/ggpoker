859d297db5a691dbc166bb2d0c2a3b4d0d898b02
Habba002 <habib.bushra002@gmail.com>
2023-10-08 06:55:07 +0300

    initiate grpc p2p network

---
+++ README.md
@@ -0,0 +1,5 @@
+# gRPC setup
+1. sudo apt install -y protobuf-compiler
+2. go get -u google.golang.org/protobuf/cmd/protoc-gen-go-grpc
+3. go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+4. make proto
---
+++ p2p/node.go
@@ -0,0 +1,93 @@
+package p2p
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/habibbushira/ggpoker/proto"
+	"google.golang.org/grpc"
+)
+
+type Node struct {
+	ServerConfig
+
+	peerLock sync.RWMutex
+	peers    map[proto.GossipClient]*proto.Version
+
+	proto.UnimplementedGossipServer
+}
+
+func NewNode(cfg ServerConfig) *Node {
+	return &Node{
+		ServerConfig: cfg,
+		peers:        make(map[proto.GossipClient]*proto.Version),
+	}
+}
+
+func (n *Node) Handshake(ctx context.Context, version *proto.Version) (*proto.Version, error) {
+	client, err := makeGrpcClientConn(version.ListenAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	n.addPeer(client, version)
+
+	return n.getVersion(), nil
+}
+
+func (n *Node) addPeer(c proto.GossipClient, v *proto.Version) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+	n.peers[c] = v
+
+	fmt.Printf("new player connected: we=%s, remote=%s\n", n.ListenAddr, v.ListenAddr)
+}
+
+func (n *Node) getVersion() *proto.Version {
+	return &proto.Version{
+		Version:    "GGPOKER-0.0.1",
+		ListenAddr: n.ListenAddr,
+	}
+}
+
+func (n *Node) Connect(addr string) error {
+	client, err := makeGrpcClientConn(addr)
+	if err != nil {
+		return err
+	}
+
+	hs, err := client.Handshake(context.TODO(), n.getVersion())
+	if err != nil {
+		return nil
+	}
+
+	n.addPeer(client, hs)
+	return nil
+
+}
+
+func (n *Node) Start() error {
+	grpcServer := grpc.NewServer()
+	proto.RegisterGossipServer(grpcServer, n)
+
+	ln, err := net.Listen("tcp", n.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("started new poker game server: port=%s, variant=%s, maxPlayers=%d\n", n.ListenAddr, n.GameVariant, n.MaxPlayers)
+
+	return grpcServer.Serve(ln)
+}
+
+func makeGrpcClientConn(addr string) (proto.GossipClient, error) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	client := proto.NewGossipClient(conn)
+	return client, nil
+}
